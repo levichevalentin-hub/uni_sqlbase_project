3edd9426ebf9b73886f427743fb4a78a07522f7d
valery <valeriylegendar@gmail.com>
2026-07-18 16:28:06 +0200

    load-show-teacher-student

---
+++ database/tables/teacher.py
@@ -0,0 +1,27 @@
+
+from sqlalchemy import UniqueConstraint, Column, Integer, String, Date
+
+from database.tables.base import Base
+
+class Teacher(Base):
+    __tablename__ = "Teacher"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    second_name = Column(String, nullable=False)
+    birthday = Column(Date, nullable=False)
+    jmbg = Column(Integer, nullable=False)
+    address = Column(String, nullable=False)
+    subject = Column(String, nullable = False)
+
+    __table_args__ = (
+        UniqueConstraint(
+            'first_name', 
+            'second_name', 
+            'birthday', 
+            name='uq_teacher_full_name_birthday'  # Имя индекса в базе данных
+        ),
+    )
+
+    def __repr__(self):
+        return f"Teacher (id = {self.id}, first_name = {self.first_name}, second_name = {self.second_name}, birthday = {self.birthday},  jmbg = {self.jmbg}, address = {self.address}, subject = {self.subject})"
---
+++ src/sql_engine.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import json
+from pathlib import Path
+
+from sqlalchemy import UniqueConstraint, func, create_engine, Column, Integer, String, Date, select
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+def load_table(session, file_path, model_class, datetime_fields=[]):
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as f:
+            data = json.load(f)
+            
+            if isinstance(data, list):
+                for item in data:
+                    for field in datetime_fields:
+                        if field in item and isinstance(item[field], str):
+                            item[field] = datetime.strptime(item[field], "%Y-%m-%d").date()
+                    # Создаем объект модели и добавляем в сессию
+                    record = model_class(**item)
+                    session.add(record)
+                
+                session.commit()
+                print(f"Data for {model_class.__tablename__} has been successfully loaded.")
+            else:
+                print(f"Error: JSON file format is incorrect. Expected a list of objects.")
+    except Exception as ex:
+        print(f"Error encountered during loading {model_class.__tablename__} data: {ex}")
+
+def display_table(session, model_class):
+    try:
+        records = session.query(model_class).all()
+        
+        print(f"\n--- Table: {model_class.__tablename__} ({len(records)} records) ---")
+        
+        if not records:
+            print("Table is empty.")
+            return
+
+        for record in records:
+            print(record)
+            
+    except Exception as ex:
+        print(f"Error displaying {model_class.__tablename__} data! Details: {ex}")
