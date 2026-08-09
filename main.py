3baca9d85841a7e7d9c8da02465d377dd2421a7b
valery <valeriylegendar@gmail.com>
2026-08-09 13:18:07 +0200

    use main file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Date, select
 from sqlalchemy.orm import sessionmaker
 
 
-from src.sql_engine import display_table, load_table
+from database.sql_engine import display_table, load_table
 
 from database.tables.school_class import SchoolClass
 
@@ -20,7 +20,7 @@ from database.tables.student_in_class import StudInClass
 
 
 # TODO: Properly connect database
-engine = create_engine("sqlite:///UNI_SQL1.db")
+engine = create_engine("sqlite:///UNI_SQL3.db")
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -28,20 +28,23 @@ session = Session()
 
 #region
 
+load_table(session, "data/students.json", Student, datetime_fields=['birthday'])
+display_table(session, Student)
+
 load_table(session, "data/school_classes.json", SchoolClass)
 display_table(session, SchoolClass)
 
-# load_table(session, "data/subject.json", Subject, datetime_fields=['birthday'])
-# display_table(session, Subject)
+load_table(session, "data/subjects.json", Subject)
+display_table(session, Subject)
 
-# load_table(session, "data/class.json", Class, datetime_fields=['birthday'])
-# display_table(session, Class)
+load_table(session, "data/students_in_classes.json", StudInClass)
+display_table(session, StudInClass)
 
-# load_table(session, "data/student_in_class.json", StudInClass, datetime_fields=['birthday'])
-# display_table(session, StudInClass)
+load_table(session, "data/teachers.json", Teacher, datetime_fields=['birthday'])
+display_table(session, Teacher)
 
-# load_table(session, "data/teachers.json", Teacher, datetime_fields=['birthday'])
-# display_table(session, Teacher)
+load_table(session, "data/grades_in_classes.json", GradeInClass, datetime_fields=['created_at'])
+display_table(session, GradeInClass)
 
 #endregion
 
